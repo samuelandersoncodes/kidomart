742e30983e49629da8c5916b93312889b0bd5f32
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-27 19:44:46 +0000

    refactor: Alter slug field for relevance

--- category/models.py
+++ category/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     """
    
     category_name = models.CharField(max_length=35, unique=True)
-    slug = models.CharField(max_length=60, unique=True)
+    slug = models.SlugField(max_length=60, unique=True)
     description = models.TextField(max_length=100, blank=True)
     category_image = models.ImageField(upload_to='images/categories', blank=True)
 
