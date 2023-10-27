06934ea06a0e7dbfd2969595dc3b665f0b6d09c7
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-27 17:50:04 +0000

    feat: Customize admin display fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    """
+    Customizes the display of admin user fields
+    It displayes the date joined and last login fields as readonly
+    and also leaves the password field as readonly
+    """
+    list_display = ('first_name', 'last_name', 'username', 'email', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
