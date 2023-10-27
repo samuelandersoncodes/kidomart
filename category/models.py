7a873b5846752d528e3730dbaa81d73a0166e3d7
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-27 19:12:03 +0000

    feat: Add model for categories

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """
+    Creates category fields
+    """
+    class Meta:
+        # Correctly pluralize the category word
+        verbose_name_plural = 'Categories'
+
+    category_name models.Charfield(max_length=35, unique=True)
+    slug = models.Charfield(max_length=60, unique=True)
+    description = models.TextField(max_length=100, blank=True)
+    category_image = models.ImageField(updload='images/categories', blank=True)
+
+    def __str__(self):
+        return self.category_name
