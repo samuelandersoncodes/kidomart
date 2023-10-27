b1699225909dd91615e8bddaab179f404c0feb3f
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-27 19:25:10 +0000

    refactor: Refactor category model to fix errors

--- category/models.py
+++ category/models.py
@@ -4,14 +4,15 @@ class Category(models.Model):
     """
     Creates category fields
     """
+   
+    category_name = models.CharField(max_length=35, unique=True)
+    slug = models.CharField(max_length=60, unique=True)
+    description = models.TextField(max_length=100, blank=True)
+    category_image = models.ImageField(upload_to='images/categories', blank=True)
+
     class Meta:
         # Correctly pluralize the category word
         verbose_name_plural = 'Categories'
-
-    category_name models.Charfield(max_length=35, unique=True)
-    slug = models.Charfield(max_length=60, unique=True)
-    description = models.TextField(max_length=100, blank=True)
-    category_image = models.ImageField(updload='images/categories', blank=True)
 
     def __str__(self):
         return self.category_name
