dbd1854b31211f9ccf6dbbeb30cf1395a9c36a2f
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-27 21:17:16 +0000

    feat: Customize product display in admin

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Displays specified fields and prepopulates slug
+    list_display = (
+        'product_name',
+        'price',
+        'stock',
+        'category',
+        'created_date',
+        'modified_date',
+        'is_available'
+    )
+    prepopulated_fields = {
+        'slug': ('product_name',)
+    }
+
+
+admin.site.register(Product, ProductAdmin)
