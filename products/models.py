8a4605ba0a19182f16c20056b35fede84ae2a2c5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-28 09:40:43 +0000

    fix: Modify product model to fix errors

--- products/models.py
+++ products/models.py
@@ -6,13 +6,13 @@ class Product(models.Model):
     Creates fields for products 
     """
     product_name = models.CharField(max_length=30,  unique=True)
-    slug = models.SlugField(max_length='60', unique=True)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, default='other')
+    slug = models.SlugField(max_length=60, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='other')
     item_id = models.CharField(max_length=60, null=True, blank=True)
     description = models.TextField(max_length=60, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(upload_to=images/static, null=False, blank=False)
+    image = models.ImageField(upload_to='products/', null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     stock = models.IntegerField()
