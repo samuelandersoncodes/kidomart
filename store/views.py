cf103ab379bc5f9e979cc741e3361535e26bcccc
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-10-28 11:11:34 +0000

    refactor: Modify home view to show products

--- store/views.py
+++ store/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from products.models import Product
+
 
 def home(request):
     """View for home page"""
+    products = products.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
     return render(request, 'index.html')
