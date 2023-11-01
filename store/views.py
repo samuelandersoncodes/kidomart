00689cb9233deae28e346fd6cb52a76387f88feb
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-01 14:04:16 +0000

    refactor: Modify home view for url sorted categories

--- store/views.py
+++ store/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
+from category.models import Category
 
 
-def home(request):
-    """View for home page"""
-    products = Product.objects.all().filter(is_available=True)
+def home(request, category_slug=None):
+    """
+    View for home page
+    the function also displays url sorted category of products
+    """
+
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     context = {
         'products': products,
+        'product_count': product_count,
     }
     return render(request, 'index.html', context)
