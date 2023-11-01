cbffb1bd80d79e6d890db4db73ac7e22aa86c0d3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-01 18:21:38 +0000

    feat: Add context processor to categorize products

---
+++ category/context_processors.py
@@ -0,0 +1,10 @@
+from .models import Category
+
+def menu_links(request):
+    """
+    This function gets all categories
+    stores them into various category links
+    and returns it in dictionary context
+    """ 
+    links = Category.objects.all()
+    return dict(links=links)
