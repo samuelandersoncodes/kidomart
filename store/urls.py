6a9697a647c76293743c75a46c6d00184f711cf3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-08 17:41:11 +0000

    build: Configure product detail url pattern

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:category_slug>/', views.home, name='products_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
