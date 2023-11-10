dc834033e9a96064fe64967cb9904f9d152379aa
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-10 21:50:56 +0000

    build: Include cart url at project level

--- kido_mart/urls.py
+++ kido_mart/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('carts.urls')),
     path('', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
