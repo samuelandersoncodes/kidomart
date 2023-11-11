e8f64c008ec84cd7846ce41e130b405ac0e569c6
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-11 15:30:44 +0000

    refactor: Adjust cart url for id change

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('add_cart/<str:item_id>/', views.add_cart, name='add_cart'),
 ]
