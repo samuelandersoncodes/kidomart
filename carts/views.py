9a2da8a14b5497c522716d463a0423e0151d0ccf
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-11 12:28:22 +0000

    feat: Add function for adding product to cart

--- carts/views.py
+++ carts/views.py
@@ -15,6 +15,42 @@ def _cart_id(request):
     return cart
 
 
+def add_cart(request, product_id):
+    """
+    View for adding a product to the cart
+    It gets the product object based on the provided product ID
+    And tries to retrieve an existing cart for the current session
+    If no cart exists, it creates a new one with the current session's cart ID
+    And saves existing or newly created cart
+    It also tries to retrieve an existing cart item for the given product and cart
+    If the item exists, it increments its quantity
+    If no cart item exists, it creates a new one with quantity 1
+    And finally Redirect the user to the 'cart' page after adding the item
+    """
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart,
+        )
+        cart_item.save()
+
+    return redirect('cart')
+
+
 def cart(request):
     # View to display cart template
     return render(request, 'cart.html')
