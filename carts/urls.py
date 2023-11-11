61b8215b2a7b500371ef1d1cfd1fca0ccaa6cd8a
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-11 21:33:29 +0000

    build: Wire up cart item deletion url

--- carts/urls.py
+++ carts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_cart/<str:item_id>/', views.add_cart, name='add_cart'),
     path('remove_cart/<str:item_id>/', views.remove_cart, name='remove_cart'),
+    path('delete_cart_item/<str:item_id>/', views.delete_cart_item, name='delete_cart_item'),
 ]
