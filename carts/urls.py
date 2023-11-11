61c277911fe5d35b3429c10ef9125dcbea70682b
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-11 21:09:46 +0000

    build: Wire up item quantity removal url

--- carts/urls.py
+++ carts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_cart/<str:item_id>/', views.add_cart, name='add_cart'),
+    path('remove_cart/<str:item_id>/', views.remove_cart, name='remove_cart'),
 ]
