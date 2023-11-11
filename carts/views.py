e55e277b85b061f082c273f872ab7601b2ffbf22
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-11 12:25:42 +0000

    feat: Add function for getting cart id

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from products.models import Product
+from .models import Cart, CartItem
+
+
+def _cart_id(request):
+    """
+    Helper function to get or create a unique cart ID for the current session 
+    It checks if a cart ID is already present in the session
+    If not, it creates a new session and use its key as the cart id
+    """
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
 
 
 def cart(request):
-    # cart view
+    # View to display cart template
     return render(request, 'cart.html')
