e3fc0bf8a3c87ddff503bd55125a52e28afeffd9
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-13 21:48:49 +0000

    refactor: Alter cart urls for view modification

--- carts/urls.py
+++ carts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    path('add_cart/<str:item_id>/', views.add_cart, name='add_cart'),
-    path('remove_cart/<str:item_id>/', views.remove_cart, name='remove_cart'),
+    path('add_to_cart/<str:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<str:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('delete_cart_item/<str:item_id>/', views.delete_cart_item, name='delete_cart_item'),
 ]
