321e1df08cb1a1a1c97150e6bd93935f81cc14c4
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-14 20:57:25 +0000

    refactor: Alter home view for redundancy

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from products.models import Product
 from category.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
 
 
 def home(request, category_slug=None):
@@ -11,23 +13,20 @@ def home(request, category_slug=None):
     their respective category names and product counts
     """
     selected_category = None
-    categories = None
     products = None
 
     if category_slug != None:
-        categories = get_object_or_404(Category, slug=category_slug)
         selected_category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(category=selected_category, is_available=True)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
         product_count = products.count()
-
     context = {
         'products': products,
         'product_count': product_count,
         'selected_category': selected_category,
-        'categories': categories,
+        
     }
     return render(request, 'index.html', context)
 
@@ -36,10 +35,12 @@ def product_detail(request, category_slug, product_slug):
     # product detail view
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Product.DoesNotExist:
         raise Http404("Sorry! This product does not exist")
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'product_detail.html', context)
