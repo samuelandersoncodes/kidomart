d0277539259f433d129529931150fec311211477
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-15 20:19:16 +0000

    fix: Fix empty cart button click error

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def _cart_id(request):
@@ -103,6 +104,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
     And renders the template with the calculated context
     """
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True).order_by('-id')
         for cart_item in cart_items:
@@ -113,7 +116,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (1 * total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
