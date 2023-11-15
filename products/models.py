288a0ec9297790c7a371106ed9391ec5f9864cde
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-15 21:10:19 +0000

    feat: Create product variation model

--- products/models.py
+++ products/models.py
@@ -9,17 +9,21 @@ def generate_item_id():
     unique_id = str(uuid.uuid4())  # Generate a unique identifier
     return f'{prefix}-{unique_id}'
 
+
 class Product(models.Model):
     """
     Creates fields for products 
     """
     product_name = models.CharField(max_length=30,  unique=True)
     slug = models.SlugField(max_length=60, unique=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='other')
-    item_id = models.CharField(max_length=50, unique=True, default=generate_item_id)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, default='other')
+    item_id = models.CharField(
+        max_length=50, unique=True, default=generate_item_id)
     description = models.TextField(max_length=60, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(upload_to='products/', null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
@@ -32,3 +36,25 @@ class Product(models.Model):
     def __str__(self):
         # Returns actual product name
         return self.product_name
+
+
+# Variation choices
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+
+class ProductVariation(models.Model):
+    """
+    Creates fields for product variations
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(
+        max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.product
