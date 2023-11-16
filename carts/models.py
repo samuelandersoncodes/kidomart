471fc1ae92f19388951978cfb4000c17bbbcf2c4
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-16 16:38:56 +0000

    feat: Feature variations in cart item

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from products.models import Product
+from products.models import Product, ProductVariation
 
 
 class Cart(models.Model):
@@ -15,6 +15,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     # Model representing item within the shopping cart
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(ProductVariation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
