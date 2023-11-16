866b7c4a67ff7b95a31fc704031df0df16fdc778
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-16 16:31:23 +0000

    refactor: Modify variation class string representation

--- products/models.py
+++ products/models.py
@@ -69,5 +69,5 @@ class ProductVariation(models.Model):
 
     objects = VariationManager()
 
-    def __unicode__(self):
-        return self.product
+    def __str__(self):
+        return self.variation_value
