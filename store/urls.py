82e9e48091343f04e1aa017cea2ebc35285947d9
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-17 21:29:19 +0000

    build: Wire up search url

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
     path('<slug:category_slug>/', views.home, name='products_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:product_slug>/',
+         views.product_detail, name='product_detail'),
 ]
