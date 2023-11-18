38ab133e42f1b97745e457cad0a28e6dcae3b59a
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-18 11:30:45 +0000

    build: Include accounts urls in project urls

--- kido_mart/urls.py
+++ kido_mart/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
