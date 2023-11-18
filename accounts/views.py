2b650646010e1de189ef9bbbae17d7e40fc64eed
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-18 11:21:44 +0000

    feat: Add acounts login view

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def register(request):
     # Register view
     return render(request, 'accounts/register.html')
+
+def login(request):
+    # login view
+    return render(request, 'accounts/login.html')
