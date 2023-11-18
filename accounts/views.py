a8d3ded854b5f34de0fb6b51c0b35955b3e5a0fd
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-18 20:10:05 +0000

    feat: Format register view to fix indent error

--- accounts/views.py
+++ accounts/views.py
@@ -9,34 +9,34 @@ def register(request):
     It validates the registration form, creates a new user account,
     and saves it to the database if the form is valid.
     """
-       if request.method == 'POST':
-            form = RegistrationForm(request.POST)
-            if form.is_valid():
-                first_name = form.cleaned_data['first_name']
-                last_name = form.cleaned_data['last_name']
-                email = form.cleaned_data['email']
-                tel = form.cleaned_data['tel']
-                password = form.cleaned_data['password']
-                username = email.split("@")[0]
-                user = Account.objects.create_user(
-                    first_name=first_name,
-                    last_name=last_name,
-                    email=email,
-                    username=username,
-                    password=password
-                )
-                user.tel = tel
-                user.save()
-        else:
-            form = RegistrationForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'accounts/register.html', context)
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            tel = form.cleaned_data['tel']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            user.tel = tel
+            user.save()
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
 
 
 def login(request):
-       # login view
+    # login view
     return render(request, 'accounts/login.html')
 
 
