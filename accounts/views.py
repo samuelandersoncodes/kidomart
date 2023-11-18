dac16bbb23fc37eeca11021698d8f56372d3221b
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-18 13:18:48 +0000

    feat: Add form to the register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+
 
 def register(request):
     # Register view
-    return render(request, 'accounts/register.html')
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
 
 def login(request):
     # login view
     return render(request, 'accounts/login.html')
+
 
 def logout(request):
     # logout view
