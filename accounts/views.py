67004276666a3a9c1a759a36bb8d40d7a9359c56
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-18 20:03:25 +0000

    feat: Validate register form to create user

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,42 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 
 
 def register(request):
-    # Register view
-    form = RegistrationForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'accounts/register.html', context)
+    """
+    Handles user registration
+    It validates the registration form, creates a new user account,
+    and saves it to the database if the form is valid.
+    """
+       if request.method == 'POST':
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                first_name = form.cleaned_data['first_name']
+                last_name = form.cleaned_data['last_name']
+                email = form.cleaned_data['email']
+                tel = form.cleaned_data['tel']
+                password = form.cleaned_data['password']
+                username = email.split("@")[0]
+                user = Account.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    username=username,
+                    password=password
+                )
+                user.tel = tel
+                user.save()
+        else:
+            form = RegistrationForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'accounts/register.html', context)
 
 
 def login(request):
-    # login view
+       # login view
     return render(request, 'accounts/login.html')
 
 
