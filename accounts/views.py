ba37c84cda63a3855652ce668ce88cdcddc263a6
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-19 16:40:16 +0000

    feat: Add logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -58,6 +59,9 @@ def login(request):
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
     # logout view
-    return
+    auth.logout(request)
+    messages.success(request, 'You just logged out!')
+    return redirect('login')
