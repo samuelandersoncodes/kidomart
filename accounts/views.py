80d8d2ffac7d6668ec8b1f03679e98fdf600d9c3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-20 14:06:36 +0000

    feat: Add logic to send activation email

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,12 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 
 def register(request):
@@ -10,6 +16,7 @@ def register(request):
     Handles user registration
     It validates the registration form, creates a new user account,
     and saves it to the database if the form is valid.
+    an email is the sent to the user for account activation
     """
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -29,6 +36,18 @@ def register(request):
             )
             user.tel = tel
             user.save()
+
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your Kido account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
             messages.success(request, 'Your Kido Registration is Successful!')
             return redirect('register')
     else:
@@ -65,3 +84,4 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You just logged out!')
     return redirect('login')
+
