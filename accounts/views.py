13451c60405754b3ed7f351fd06a7cb517cd2047
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-21 17:14:28 +0000

    feat: Add dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -109,3 +109,8 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation link')
         return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
