ec9d02127ecf3313ebe3afa3343be3d22a035d60
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-21 20:08:46 +0000

    feat: Add view for forgot password

--- accounts/views.py
+++ accounts/views.py
@@ -114,3 +114,28 @@ def activate(request, uidb64, token):
 @login_required(login_url='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            current_site = get_current_site(request)
+            mail_subject = 'Please reset your password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            messages.success(
+                request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'This account does not exist')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
