07a9307cd0d2101b78dfe772c36e7ecff4e5c3a4
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-21 20:44:32 +0000

    build: Configure forgot password url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
