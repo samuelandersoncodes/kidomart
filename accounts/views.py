07423d9cc6feae7a9bac610d67169e1fe9a73b3f
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-22 17:40:35 +0000

    fix: Correct carts import to fix module error

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
-from carts.view import _cart_id
+from carts.views import _cart_id
 from carts.models import Cart, CartItem
 
 
