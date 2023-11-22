ff26f0a0e64d20eeb775d4aaa554e07c7aeaee03
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-22 10:12:00 +0000

    feat; Add checkout url path

--- carts/urls.py
+++ carts/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add_to_cart/<str:item_id>/', views.add_to_cart, name='add_to_cart'),
-    path('remove_from_cart/<str:item_id>/<int:remove_cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('delete_cart_item/<str:item_id>/<int:remove_cart_item_id>/', views.delete_cart_item, name='delete_cart_item'),
+    path('remove_from_cart/<str:item_id>/<int:remove_cart_item_id>/',
+         views.remove_from_cart, name='remove_from_cart'),
+    path('delete_cart_item/<str:item_id>/<int:remove_cart_item_id>/',
+         views.delete_cart_item, name='delete_cart_item'),
+    path('checkout/' views.checkout, name='checkout'),
 ]
