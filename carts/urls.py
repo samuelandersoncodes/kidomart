e545a2b65714ae4e7379679645e5eb5a59c07230
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-22 10:35:21 +0000

    fix; Modify checkout url to fix error

--- carts/urls.py
+++ carts/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
     path('add_to_cart/<str:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<str:item_id>/<int:remove_cart_item_id>/',
          views.remove_from_cart, name='remove_from_cart'),
     path('delete_cart_item/<str:item_id>/<int:remove_cart_item_id>/',
          views.delete_cart_item, name='delete_cart_item'),
-    path('checkout/' views.checkout, name='checkout'),
 ]
