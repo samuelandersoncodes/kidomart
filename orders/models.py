8022466a0729e4b0eb4640910ef9aa20e7adc7d4
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-23 18:56:58 +0000

    feat: Add payment model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    # Payment model
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    payment_id = models.Charfield(max_length=100)
+    payment_method = models.Charfield(max_length=100)
+    amount_paid = models.Charfield(max_length=100)
+    status = models.Charfield(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(slef):
+        # Payment id string representation
+        return self.payment_id
