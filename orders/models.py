f9973f47c63c5f7b5b071e7821ac0555395b8b5c
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-23 19:08:28 +0000

    feat: Import accout for user field

--- orders/models.py
+++ orders/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from accounts.models import Account
 
 class Payment(models.Model):
     # Payment model
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    payment_id = models.Charfield(max_length=100)
-    payment_method = models.Charfield(max_length=100)
-    amount_paid = models.Charfield(max_length=100)
-    status = models.Charfield(max_length=100)
+    payment_id = models.CharField(max_length=100)
+    payment_method = models.CharField(max_length=100)
+    amount_paid = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(slef):
