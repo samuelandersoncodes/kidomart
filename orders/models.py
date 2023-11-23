594098a62f1cdbeea1eb681544386a75c7937f01
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-23 19:52:01 +0000

    feat: Add the order model

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import Account
 
+
 class Payment(models.Model):
     # Payment model
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -13,3 +14,40 @@ class Payment(models.Model):
     def __str__(slef):
         # Payment id string representation
         return self.payment_id
+
+
+class Order(models.Model):
+    # Order model
+    STATUS = (
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    )
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
+    order_number = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    tel = models.CharField(max_length=15)
+    email = models.EmailField(max_length=60, unique=True)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    order_note = models.CharField(max_length=100, blank=True)
+    order_total = models.FloatField()
+    tax = models.FloatField()
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(max_length=20, blank=True)
+    is_ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(slef):
+        # user first name string representation
+        return self.user.first_name
+
+
+
