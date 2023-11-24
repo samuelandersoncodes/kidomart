2a3dde107bcd4de9944e00889ea7b36bf8c031bd
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-24 13:09:35 +0000

    feat: Add order form

---
+++ orders/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            'first_name',
+            'last_name',
+            'tel',
+            'email',
+            'address_line_1',
+            'address_line_2',
+            'country',
+            'state',
+            'city',
+            'order_note'
+        ]
