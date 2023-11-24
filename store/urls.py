8a28a0f1450fe3ec2bfd31b1d21ff9591340098d
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-24 21:16:33 +0000

    refactor: Modify stores url for robustness

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
-    path('<slug:category_slug>/', views.home, name='products_category'),
-    path('<slug:category_slug>/<slug:product_slug>/',
+    path('category/<slug:category_slug>/',
+         views.home, name='products_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/',
          views.product_detail, name='product_detail'),
 ]
