409df571e10856658e8ff9aa30a1419e27806904
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-24 18:04:35 +0000

    feat: Add order view context

--- orders/views.py
+++ orders/views.py
@@ -58,7 +58,16 @@ def place_order(request, total=0, quantity=0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+            order = Order.objects.get(
+                user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            return render('payments.html', context)
         else:
             return redirect('checkout')
     return redirect('checkout')
