1ea210b77d72fa8ce29a7adf0225ff0fce906fe1
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-24 09:33:35 +0000

    feat: Add orders url to main url pattern

--- kido_mart/urls.py
+++ kido_mart/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
     path('', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
