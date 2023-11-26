824c5d4a5cb1b0c5c5b601aa7026f7cb48c641b5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-26 19:29:51 +0000

    refator: Modify code for robustness

--- static/js/paypal.js
+++ static/js/paypal.js
@@ -1,4 +1,4 @@
-// jshint esversion: 6
+// // jshint esversion: 6
 
 // Paypal integration
 // This function loads the PayPal SDK dynamically
@@ -7,19 +7,22 @@ function loadPayPalScript(clientID, callback) {
     script.src = `https://www.paypal.com/sdk/js?client-id=${clientID}&currency=USD`;
     script.async = true;
     script.defer = true;
-    script.onload = callback;
+    script.onload = function () {
+        callback();
+    };
     document.head.appendChild(script);
 }
 
 // Function to set up the PayPal button
 function setupPayPalButton() {
     paypal.Buttons({
+        env: 'sandbox',
         createOrder: function (data, actions) {
-            // Set up the transaction
+            // Set up the transaction with the global variable as the amount
             return actions.order.create({
                 purchase_units: [{
                     amount: {
-                        value: '11.00'
+                        value: totalAmount
                     }
                 }]
             });
@@ -28,8 +31,13 @@ function setupPayPalButton() {
             // Capture the funds from the transaction
             return actions.order.capture().then(function (details) {
                 // Display a success message to the user
-                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');               
+            }).catch(function (error) {
+                // Log the detailed error information
+                console.error('Error capturing funds:', error);
+                console.error('Error details:', error.details.originalError.response);
+                // Handle the error or notify the user appropriately
             });
-        }
+        },
     }).render('#paypal-button-container');
 }
