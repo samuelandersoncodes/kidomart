7308ab131756587762a4b508a6e4535fcb573f96
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-27 11:55:07 +0000

    feat: Customize order list display

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,33 @@ from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 
 
-admin.site.register(Order)
+class OderAdmin(admin.ModelAdmin):
+    list_display = [
+        'order_number',
+        'full_name',
+        'email',
+        'tel',
+        'city',
+        'order_total',
+        'tax',
+        'status',
+        'is_ordered',
+        'created_at'
+    ]
+    list_filter = [
+        'status',
+        'is_ordered'
+    ]
+    search_fields = [
+        'order_number',
+        'first_name',
+        'last_name',
+        'email',
+        'tel'
+    ]
+    list_per_page = 25
+
+
+admin.site.register(Order, OderAdmin)
 admin.site.register(OrderProduct)
 admin.site.register(Payment)
