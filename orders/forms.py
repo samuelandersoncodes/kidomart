6aea58b26b1e2044a258d471b67b0da69a5bea20
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-28 15:37:15 +0000

    feat: Add placeholders and focus to order form

--- orders/forms.py
+++ orders/forms.py
@@ -5,7 +5,7 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = [
+        fields = (
             'first_name',
             'last_name',
             'tel',
@@ -16,4 +16,32 @@ class OrderForm(forms.ModelForm):
             'state',
             'city',
             'order_note'
-        ]
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        This method adds placeholders and classes,
+        remove auto-generated labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'tel': 'Phone Number',
+            'email': 'Email Address',
+            'address_line_1': 'Address Line 1',
+            'address_line_2': 'Address Line 2',
+            'country': 'Country',
+            'state': 'State',
+            'city': 'State',
+            'order_note': 'Leave a Note',
+        }
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
