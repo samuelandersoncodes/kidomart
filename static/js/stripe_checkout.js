b847ff05d5cb852e06663f6cd9a148ef9ff91f94
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-28 12:44:56 +0000

    feat: Add stripe core variables

---
+++ static/js/stripe_checkout.js
@@ -0,0 +1,7 @@
+// jshint esversion: 6
+
+// Stripe keys and elements
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
