a3535de611cd24d7bc2c5da825b4eaac00649146
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-11-30 08:46:51 +0000

    chore: Add url for checkout success

--- carts/urls.py
+++ carts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('checkout_success/', views.checkout_success, name='checkout_success'),
     path('add_to_cart/<str:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<str:item_id>/<int:remove_cart_item_id>/',
          views.remove_from_cart, name='remove_from_cart'),
