4708e2e2c9a294ad11a77854cbf53b8d1be9d8b5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-02 13:10:18 +0000

    feat: Register review rating with admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductVariation
+from .models import Product, ProductVariation, ReviewRating
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -38,3 +38,5 @@ class ProductVariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 
 admin.site.register(ProductVariation, ProductVariationAdmin)
+
+admin.site.register(ReviewRating)
