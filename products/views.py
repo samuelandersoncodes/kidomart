a949510ff7ff2b77090d17d612aa91efc0d32833
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-02 17:43:15 +0000

    feat: Add submit review view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Product, ReviewRating
+from .forms import ReviewForm
 
-# Create your views here.
+
+def submit_review(request, item_id):
+    """
+    This function handles the submission of product reviews.
+    It retrieves the URL of the previous page using the HTTP_REFERER header.
+    Checks if the form submission method is POST.
+    If a review by the current user for the specified product already exists,
+    It loads the existing review into the ReviewForm for an update.
+    If no existing review is found, it creates a new review rating instance
+    using the ReviewForm data.
+    """
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(
+                user__id=request.user.id, product__id=item_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(
+                request, 'Your review has been successfully updated')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = item_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thanks! Your review is added.')
+                return redirect(url)
