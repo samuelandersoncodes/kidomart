9a4fd801a031e7b271413c2db4ffebb8183c806b
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-03 17:11:25 +0000

    feat: Add edit profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('reset_password_validate/<uidb64>/<token>/',
          views.reset_password_validate, name='reset_password_validate'),
     path('my_orders/', views.my_orders, name='my_orders'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
 ]
