c9c487d400b10e14d8f779f24c5f20559a013495
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-03 16:47:18 +0000

    feat: Register user profile in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import Account, UserProfile
 
 
 class AccountAdmin(UserAdmin):
@@ -9,7 +9,8 @@ class AccountAdmin(UserAdmin):
     It displayes the date joined and last login fields as readonly
     and also leaves the password field as readonly
     """
-    list_display = ('first_name', 'last_name', 'username', 'email', 'last_login', 'date_joined', 'is_active')
+    list_display = ('first_name', 'last_name', 'username',
+                    'email', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('date_joined', 'last_login')
     ordering = ('-date_joined',)
@@ -20,3 +21,4 @@ class AccountAdmin(UserAdmin):
 
 
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
