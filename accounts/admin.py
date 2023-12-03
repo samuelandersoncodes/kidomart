277add8bb82ab54c334e7de9ed9dd33c16a88e5c
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-03 16:58:33 +0000

    fix: Import format html to fix type error

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Account, UserProfile
+from django.utils.html import format_html
 
 
 class AccountAdmin(UserAdmin):
