a45dfd9466ee02d7f7044f07f1a386c66a0e6a9a
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-04 12:14:51 +0000

    fix: Remove bracket to fix syntax error

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,4 +17,3 @@ urlpatterns = [
     path('change_password/', views.change_password, name='change_password'),
     path('order_detail/<int:order_id>', views.order_detail, name='order_detail'),
 ]
-]
