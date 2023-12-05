0bdfb4e67850978cc55d0b78b14ee8ae95a097a8
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-05 21:47:45 +0000

    fix: Correct message syntax to fix error

--- accounts/views.py
+++ accounts/views.py
@@ -59,7 +59,7 @@ def register(request):
             send_email.send()
             return redirect('/accounts/login/?command=verification&email='+email)
         else:
-            messages.error(request. 'Carefully check your details and try again')
+            messages.error(request, 'Carefully check your details and try again')
             form = RegistrationForm()
     else:
         form = RegistrationForm()
