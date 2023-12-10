8dc03d99039e2e35364b87f6272fe4b74dcda35d
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-10 23:53:18 +0000

    test: Add user account test

--- accounts/test_models.py
+++ accounts/test_models.py
@@ -47,3 +47,42 @@ class MyAccountManagerTest(TestCase):
         self.assertTrue(superuser.is_active)
         self.assertTrue(superuser.is_staff)
         self.assertTrue(superuser.is_superadmin)
+
+
+class AccountModelTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='testpassword',
+            first_name='Test',
+            last_name='User',
+        )
+
+    def test_create_user(self):
+        # Retrieve the created user
+        user = get_user_model().objects.get(email='test@example.com')
+
+        # Test that the user has the expected attributes
+        self.assertEqual(user.first_name, 'Test')
+        self.assertEqual(user.last_name, 'User')
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.check_password('testpassword'))
+        self.assertFalse(user.is_admin)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_superadmin)
+
+    def test_str_representation(self):
+        # Test the string representation of the user
+        self.assertEqual(str(self.user), 'test@example.com')
+
+    def test_has_perm(self):
+        # Test the has_perm method
+        self.assertFalse(self.user.has_perm('some_permission'))
+
+    def test_has_module_perms(self):
+        # Test the has_module_perms method
+        self.assertTrue(self.user.has_module_perms('some_label'))
