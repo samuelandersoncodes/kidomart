a49372654b83361847ea7b0b66b059dab3a11102
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-11 21:28:20 +0000

    refactor: Clean up category context processor

--- category/context_processors.py
+++ category/context_processors.py
@@ -1,10 +1,11 @@
 from .models import Category
 
+
 def menu_links(request):
     """
     This function gets all categories
     stores them into various category links
     and returns it in dictionary context
-    """ 
+    """
     links = Category.objects.all()
     return dict(links=links)
