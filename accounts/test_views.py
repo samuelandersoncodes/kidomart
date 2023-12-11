14f841498a2f0d176225564394eeea985434e1ba
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-11 00:44:43 +0000

    test: Add account activation test

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -4,6 +4,14 @@ from django.core import mail
 from django.contrib.auth import get_user_model
 from .forms import RegistrationForm
 from django.contrib.messages import get_messages
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.messages.storage.fallback import FallbackStorage
+from django.contrib.messages import get_messages
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from accounts.views import activate
+from .models import Account
 
 
 class RegisterViewTest(TestCase):
@@ -46,3 +54,77 @@ class LoginViewTest(TestCase):
         else:
             # Check if the user is redirected to the dashboard on successful login
             self.assertRedirects(response, reverse('dashboard'))
+
+
+class ActivateViewTest(TestCase):
+    # Account activation test
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = get_user_model().objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='testpassword',
+            first_name='Test',
+            last_name='User',
+        )
+        self.uidb64 = urlsafe_base64_encode(force_bytes(self.user.pk))
+        self.token = default_token_generator.make_token(self.user)
+
+    def test_activate_view_success(self):
+        url = reverse('activate', args=[self.uidb64, self.token])
+        request = self.factory.get(url)
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = activate(request, self.uidb64, self.token)
+        self.assertEqual(response.url, reverse('login'))
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.is_active)
+        # Check if success message is displayed
+        messages = [m.message for m in get_messages(request)]
+        self.assertIn('Your Kido account is activated!', messages)
+
+    def test_activate_view_invalid_link(self):
+        # Change the token to simulate an invalid link
+        invalid_token = 'invalid_token'
+        url = reverse('activate', args=[self.uidb64, invalid_token])
+        request = self.factory.get(url)
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = activate(request, self.uidb64, invalid_token)
+        self.assertEqual(response.url, reverse('register'))
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.is_active)
+        # Check if error message is displayed
+        messages = [m.message for m in get_messages(request)]
+        self.assertIn('Invalid activation link', messages)
+
+    def test_activate_view_invalid_uid(self):
+        # Change the uidb64 to simulate an invalid UID
+        invalid_uidb64 = 'invalid_uidb64'
+        url = reverse('activate', args=[invalid_uidb64, self.token])
+        request = self.factory.get(url)
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = activate(request, invalid_uidb64, self.token)
+        self.assertEqual(response.url, reverse('register'))
+        self.assertFalse(self.user.is_active)
+        # Check if error message is displayed
+        messages = [m.message for m in get_messages(request)]
+        self.assertIn('Invalid activation link', messages)
+
+    def test_activate_view_user_does_not_exist(self):
+        # Change the uidb64 to simulate a non-existent user
+        non_existent_uidb64 = 'AAAAAAAAAAA'
+        url = reverse('activate', args=[non_existent_uidb64, self.token])
+        request = self.factory.get(url)
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        response = activate(request, non_existent_uidb64, self.token)
+        self.assertEqual(response.url, reverse('register'))
+        # Check if error message is displayed
+        messages = [m.message for m in get_messages(request)]
+        self.assertIn('Invalid activation link', messages)
