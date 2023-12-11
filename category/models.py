a09917fd16ef31c403af469a53119bb926542e2c
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-11 21:31:33 +0000

    refactor: Clean up category models

--- category/models.py
+++ category/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Category(models.Model):
     """
     Creates category fields
     """
-   
+
     category_name = models.CharField(max_length=35, unique=True)
     slug = models.SlugField(max_length=60, unique=True)
     description = models.TextField(max_length=100, blank=True)
-    category_image = models.ImageField(upload_to='images/categories', blank=True)
+    category_image = models.ImageField(
+        upload_to='images/categories', blank=True)
 
     class Meta:
         # Correctly pluralize the category word
