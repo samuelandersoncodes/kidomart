6a53312c9cf312737eee691b38acd6d6f6ffae73
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-12-11 21:20:05 +0000

    refactor: Clean up carts urls

--- carts/urls.py
+++ carts/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
-    path('checkout_success/<str:order_number>', views.checkout_success, name='checkout_success'),
+    path('checkout_success/<str:order_number>',
+         views.checkout_success, name='checkout_success'),
     path('add_to_cart/<str:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<str:item_id>/<int:remove_cart_item_id>/',
          views.remove_from_cart, name='remove_from_cart'),
